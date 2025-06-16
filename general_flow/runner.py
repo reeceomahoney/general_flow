85a0ad59f9bd2cf3c7beaaf69cf3918aea7b8a6c
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-06-16 11:07:48 +0100

    fix image size

--- general_flow/runner.py
+++ general_flow/runner.py
@@ -61,7 +61,7 @@ class GeneralFlowRunner(OnPolicyRunner):
 
         # evaluate the policy class
         self.policy_cfg.pop("class_name")
-        image_shape = (64, 64, 3)
+        image_shape = (64, 64, 6)
         num_state_obs = num_obs - math.prod(image_shape)
         observation_shape = {"image": image_shape, "state": num_state_obs}
 

eff737053fb959283e23432f9743430c3ccde8d7
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-06-18 10:28:53 +0100

    add segmentation to point cloud test

--- tests/test_point_cloud.py
+++ tests/test_point_cloud.py
@@ -1,10 +1,12 @@
 import torch
 import open3d as o3d
 from pytorch3d.transforms import Transform3d
+from pytorch3d.ops import sample_farthest_points
 
 # load RGB and depth images
 rgb_img = torch.load("tests/rgb.pt")
 depth_img = torch.load("tests/depth.pt")
+seg_img = torch.load("tests/seg.pt")
 B = rgb_img.shape[0]
 device = "cuda"
 
@@ -28,15 +30,18 @@ points = torch.cat((X, Y, Z), dim=-1)
 
 # truncate points based on depth
 mask = (Z < 3.0).squeeze(-1)
-points = points[mask]
-rgb_img = rgb_img[mask]
+seg_mask = (seg_img != 15).squeeze(-1)
+points = points[mask & seg_mask]
+rgb_img = rgb_img[mask & seg_mask]
 
 # transform points to world coordinates
 t = Transform3d(device=device).rotate_axis_angle(33).translate(0, 1.5, -2.5)
 world_points = t.transform_points(points)
 
+world_points = sample_farthest_points(world_points.unsqueeze(0), K=512)[0]
+
 # visualize
 pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(world_points.cpu().numpy())
+pcd.points = o3d.utility.Vector3dVector(world_points[0].cpu().numpy())
 pcd.colors = o3d.utility.Vector3dVector(rgb_img.cpu().numpy() / 255.0)
 o3d.visualization.draw_geometries([pcd])  # type: ignore

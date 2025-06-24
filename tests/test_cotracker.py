5acf1d5e694fb2a83a4cb355559b94e91619667d
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-06-24 16:28:23 +0100

    add segm mask and script options

--- tests/test_cotracker.py
+++ tests/test_cotracker.py
@@ -1,7 +1,12 @@
+import time
+
+import imageio.v3 as iio
+import numpy as np
 import torch
 from cotracker.utils.visualizer import Visualizer
-import imageio.v3 as iio
-import time
+
+RUN_OFFLINE = True
+RUN_ONLINE = False
 
 
 def get_performance_metrics(start_time):
@@ -16,37 +21,43 @@ def get_performance_metrics(start_time):
     print(f"Total Time Taken: {total_time:.2f} seconds")
 
 
-frames = iio.imread("output.mp4", plugin="FFMPEG")  # plugin="pyav"
+frames = iio.imread("video.mp4", plugin="FFMPEG")  # plugin="pyav"
 device = "cuda"
-grid_size = 20
+grid_size = 80
 video = torch.tensor(frames).permute(0, 3, 1, 2)[None].float().to(device)  # B T C H W
 
+seg_mask = np.load("seg_masks.npy")
+seg_mask = torch.tensor(seg_mask, dtype=torch.float32, device=device)[None, None]
+
 # Load the CoTracker models
-offline_cotracker = torch.hub.load(
-    "facebookresearch/co-tracker", "cotracker3_offline"
-).to(device)  # type: ignore
-online_cotracker = torch.hub.load(
-    "facebookresearch/co-tracker", "cotracker3_online"
-).to(device)  # type: ignore
+if RUN_OFFLINE:
+    offline_cotracker = torch.hub.load(
+        "facebookresearch/co-tracker", "cotracker3_offline"
+    ).to(device)  # type: ignore
+if RUN_ONLINE:
+    online_cotracker = torch.hub.load(
+        "facebookresearch/co-tracker", "cotracker3_online"
+    ).to(device)  # type: ignore
 
 # Run Offline CoTracker:
-torch.cuda.reset_peak_memory_stats()
-start = time.time()
-pred_tracks, pred_visibility = offline_cotracker(
-    video, grid_size=grid_size
-)  # B T N 2,  B T N 1
-get_performance_metrics(start)
+if RUN_OFFLINE:
+    torch.cuda.reset_peak_memory_stats()
+    start = time.time()
+    pred_tracks, pred_visibility = offline_cotracker(
+        video, grid_size=grid_size, segm_mask=seg_mask
+    )
+    get_performance_metrics(start)
 
 # Run Online CoTracker:
-torch.cuda.reset_peak_memory_stats()
-start = time.time()
-online_cotracker(video_chunk=video, is_first_step=True, grid_size=grid_size)
-for ind in range(0, video.shape[1] - online_cotracker.step, online_cotracker.step):
-    pred_tracks, pred_visibility = online_cotracker(
-        video_chunk=video[:, ind : ind + online_cotracker.step * 2]
-    )  # B T N 2,  B T N 1
-
-get_performance_metrics(start)
+if RUN_ONLINE:
+    torch.cuda.reset_peak_memory_stats()
+    start = time.time()
+    online_cotracker(video_chunk=video, is_first_step=True, grid_size=grid_size)
+    for ind in range(0, video.shape[1] - online_cotracker.step, online_cotracker.step):
+        pred_tracks, pred_visibility = online_cotracker(
+            video_chunk=video[:, ind : ind + online_cotracker.step * 2]
+        )
+    get_performance_metrics(start)
 
 # Visualize the results
 vis = Visualizer(save_dir="./saved_videos", pad_value=0, linewidth=3)
